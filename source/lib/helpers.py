9fe24c145a7b1e5f719a01e81391c88d6109429d
liaochris email = chrisliao@cs.uchicago.edu password = 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 [push] <chrisliao@linux3.cs.uchicago.edu>
2024-10-27 09:02:26 -0500

    finished table

--- source/lib/helpers.py
+++ source/lib/helpers.py
@@ -3,15 +3,14 @@ import numpy as np
 def AddToTableList(table_list, add_list, length):
     table_list = table_list.copy()
     if len(add_list) != length:
-        add_list.extend([np.nan for i in range(length - len(add_list))])
+        add_list.extend(["" for i in range(length - len(add_list))])
     table_list.append(add_list)
 
     return table_list
 
-
-def ExportTable(filename, data, tablename):
+def ExportTable(filename, data, tablename, fmt):
     with open(filename, "w") as f:
         f.write(f'<tab:{tablename}>\n')
-        np.savetxt(f, data, fmt='%f', delimiter = '\t')
+        np.savetxt(f, data, fmt=fmt, delimiter = '\t')
         
     print(f"{filename} has been saved!")
