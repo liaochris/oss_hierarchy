44ae196ee2cf590cc36d530f6294f9400d7c617e
liaochris email = chrisliao@cs.uchicago.edu password = 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 [push] <chrisliao@linux3.cs.uchicago.edu>
2024-10-27 20:34:03 -0500

    test new lyx format

--- source/lib/helpers.py
+++ source/lib/helpers.py
@@ -3,7 +3,7 @@ import numpy as np
 def AddToTableList(table_list, add_list, length):
     table_list = table_list.copy()
     if len(add_list) != length:
-        add_list.extend(["" for i in range(length - len(add_list))])
+        add_list.extend([" " for i in range(length - len(add_list))])
     table_list.append(add_list)
 
     return table_list
